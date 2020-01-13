db6da4ed210426e362027f07284b573e71e527b3
HDVinnie <unit3d@protonmail.com>
2020-01-13 01:09:36 -0500

    (Update) Index

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,16 +24,26 @@ $(document).ready( function () {
         ],
         columnDefs: [
             {
-                targets: [8, 14],
+                targets: [7, 8, 9, 10, 11, 14],
                 render: function (data) {
+                    if (data === 'N/A') {
+                        return '<span class="label label-default" style="background-color: rgba(26, 26, 26, 1); color: #9b9b9b;">' + data + '</span>';
+                    }
                     if (data === 'Easy') {
-                        return '<span class="text-success">' + data + '</span>';
+                        return '<span class="label label-default" style="background-color: rgba(26, 26, 26, 1); color: #0FB492;">' + data + '</span>';
                     }
                     if (data === 'Medium') {
-                        return '<span class="text-warning">' + data + '</span>';
+                        return '<span class="label label-default" style="background-color: rgba(26, 26, 26, 1); color: #EE7E2A;">' + data + '</span>';
                     }
                     if (data === 'Hard') {
-                        return '<span class="text-danger">' + data + '</span>';
+                        return '<span class="label label-default" style="background-color: rgba(26, 26, 26, 1); color: #E64141;">' + data + '</span>';
+                    }
+
+                    if (data === 'Yes') {
+                        return '<span class="label label-default" style="background-color: rgba(26, 26, 26, 1); color: #50992a;">' + data + '</span>';
+                    }
+                    if (data === 'No') {
+                        return '<span class="label label-default" style="background-color: rgba(26, 26, 26, 1); color: #a05262;">' + data + '</span>';
                     }
                     return data;
                 }
